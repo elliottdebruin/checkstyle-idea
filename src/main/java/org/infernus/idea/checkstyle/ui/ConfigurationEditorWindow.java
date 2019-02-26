cbfd543b79391bf6f5199d41436cfa0816f0bd68
Ofek Inbar <ofekinbar@hotmail.com>
2019-02-25 22:01:55 -0800

    fixup! Begin Attributes Editor Dialog and add Javadoc comments to ConfigurationEditorWindow
    Begin Attributes Editor Dialog and add Javadoc comments to ConfigurationEditorWindow

--- src/main/java/org/infernus/idea/checkstyle/ui/ConfigurationEditorWindow.java
+++ src/main/java/org/infernus/idea/checkstyle/ui/ConfigurationEditorWindow.java
@@ -7,7 +7,6 @@ import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.Image;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
